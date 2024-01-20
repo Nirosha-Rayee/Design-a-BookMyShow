47c53bd86ce693d61e9d163f99a9b336e90bca3c
Nirosha-Rayee <niroshar14@gmail.com>
2024-01-20 19:22:00 +0530

    bms code

--- src/main/java/com/example/bookmyshowcode/BookmyshowcodeApplication.java
+++ src/main/java/com/example/bookmyshowcode/BookmyshowcodeApplication.java
@@ -1,5 +1,8 @@
 package com.example.bookmyshowcode;
 
+import com.example.bookmyshowcode.controllers.UserController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,13 +13,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 
-@ComponentScan(basePackages = {"com.example.bookmyshowcode.controllers", "com.example.bookmyshowcode.services", "com.example.bookmyshowcode.models", "com.example.bookmyshowcode.repositories"})
-@EnableJpaRepositories(basePackages = {"com.example.bookmyshowcode.repositories"})
-public class BookmyshowcodeApplication {
+//@ComponentScan(basePackages = {"com.example.bookmyshowcode.controllers", "com.example.bookmyshowcode.services", "com.example.bookmyshowcode.models", "com.example.bookmyshowcode.repositories"})
+//@EnableJpaRepositories(basePackages = {"com.example.bookmyshowcode.repositories"})
+public class BookmyshowcodeApplication  {
+
 
     public static void main(String[] args) {
 
         SpringApplication.run(BookmyshowcodeApplication.class, args);
     }
+
 
 }
